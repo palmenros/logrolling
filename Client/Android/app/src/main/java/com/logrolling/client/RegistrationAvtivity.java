a3f59ab812ef229d489654f6390b879aa704073f
rubgom03 <56434555+rubgom03@users.noreply.github.com>
2020-05-10 12:27:44 +0200

    PopUps a falta de revision

--- Client/Android/app/src/main/java/com/logrolling/client/RegistrationAvtivity.java
+++ Client/Android/app/src/main/java/com/logrolling/client/RegistrationAvtivity.java
@@ -1,16 +1,20 @@
 package com.logrolling.client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class RegistrationAvtivity extends AppCompatActivity {
     private EditText user, password, repPassword;
     private Switch conditions;
+    private TextView popUpMessage;
+    private ConstraintLayout popUpConfirmation, popUpError;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,12 @@ public class RegistrationAvtivity extends AppCompatActivity {
         password =(EditText)findViewById(R.id.Contrasenna);
         repPassword =(EditText)findViewById(R.id.RepContrasenna);
         conditions =(Switch)findViewById(R.id.Condiciones);
+
+        popUpError=(ConstraintLayout)findViewById(R.id.PopUpError7);
+        popUpError.setVisibility(View.INVISIBLE);
+        popUpMessage=(TextView)findViewById(R.id.messageError);
+        popUpConfirmation=(ConstraintLayout)findViewById(R.id.PopUpConfirm1);
+        popUpConfirmation.setVisibility(View.INVISIBLE);
 
     }
 
@@ -36,5 +46,18 @@ public class RegistrationAvtivity extends AppCompatActivity {
     }
     public void showPrivacyPolicy(View view){
         //Mostrar la política de privacidad
+    }
+    public void showErrorPopUp(View view){
+        // popUpMessage.setText();
+        popUpError.setVisibility(View.VISIBLE);
+    }
+    public void closeErrorPopUp(View view){
+        popUpError.setVisibility(View.INVISIBLE);
+    }
+    public void showConfirmationPopUp(View view){
+        popUpConfirmation.setVisibility(View.VISIBLE);
+    }
+    public void closeConfirmationPopUp(View view){
+        popUpConfirmation.setVisibility(View.INVISIBLE);
     }
 }
