5d4a56fecca3e40b27627d9176fd210b4a7329d6
JuanCarlosLlamas <jullamas@ucm.es>
2020-05-11 11:33:10 +0200

    Vista feedback imlementado
    Se han solucionado problemas que daba la aplicacion en distintos dispositivos y otros pequeños detalles

--- Client/Android/app/src/main/java/com/logrolling/client/MainActivity.java
+++ Client/Android/app/src/main/java/com/logrolling/client/MainActivity.java
@@ -1,23 +1,38 @@
 package com.logrolling.client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView popUpMessage;
+    private ConstraintLayout popUpError;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        popUpError=(ConstraintLayout)findViewById(R.id.PopUpError14);
+        popUpError.setVisibility(View.INVISIBLE);
+        popUpMessage=(TextView)findViewById(R.id.messageError);
     }
 
     public void advance(View view){
         //Cuando cargue
         Intent i=new Intent(this, SignInActivity.class);
         startActivity(i);
+    }
+
+    //popUpError
+    public void showErrorPopUp(View view){
+        // popUpMessage.setText();
+        popUpError.setVisibility(View.VISIBLE);
+    }
+    public void closeErrorPopUp(View view){
+        popUpError.setVisibility(View.INVISIBLE);
     }
 }
