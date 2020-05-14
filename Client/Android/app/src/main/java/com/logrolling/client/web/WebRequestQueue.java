365f4b3f77fa8cd4dcdc3d411a9735ac59d9bf39
Pedro Palacios <palmenros@gmail.com>
2020-05-14 17:32:11 +0200

    Added support for ImageLoader, used for NetworkImageView

--- Client/Android/app/src/main/java/com/logrolling/client/web/WebRequestQueue.java
+++ Client/Android/app/src/main/java/com/logrolling/client/web/WebRequestQueue.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.LruCache;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
@@ -22,18 +23,43 @@ public class WebRequestQueue {
     //Request queue that will manage all web requests
     private RequestQueue requestQueue;
 
+    //Image loader that will be used for NetworkImageView
+    private ImageLoader imageLoader;
+
     private WebRequestQueue(Context context) {
         WebRequestQueue.context = context;
 
         //Pass application context so request queue lives during all application execution
         requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+
+        //Initialize imageLoader
+        imageLoader = new ImageLoader(requestQueue,
+                    new ImageLoader.ImageCache() {
+                private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
+
+                @Override
+                public Bitmap getBitmap(String url) {
+                    return cache.get(url);
+                }
+
+                @Override
+                public void putBitmap(String url, Bitmap bitmap) {
+                    cache.put(url, bitmap);
+                }
+            });
+
     }
 
-    public static void createInstance(Context context) {
+    //Creates a new instance
+    public static synchronized void createInstance(Context context) {
+        if(instance != null) {
+            throw new IllegalStateException("WebRequestQueue instance is already created");
+        }
+
         instance = new WebRequestQueue(context);
     }
 
-    public static WebRequestQueue getInstance() {
+    public static synchronized WebRequestQueue getInstance() {
         if(instance == null) {
             throw new IllegalStateException("WebRequestQueue getInstance was called before instance creation");
         }
@@ -42,5 +68,13 @@ public class WebRequestQueue {
 
     public RequestQueue getRequestQueue() {
         return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
     }
 }
