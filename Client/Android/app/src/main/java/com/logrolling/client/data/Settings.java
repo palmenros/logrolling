913170f50b74e549455eb74c51cc6234705f98e2
Pedro Palacios <palmenros@gmail.com>
2020-05-19 00:49:52 +0200

    Created website documentation for API

--- Client/Android/app/src/main/java/com/logrolling/client/data/Settings.java
+++ Client/Android/app/src/main/java/com/logrolling/client/data/Settings.java
@@ -5,7 +5,9 @@ import com.google.gson.GsonBuilder;
 
 public class Settings {
     //private static final String baseURL = "http://192.168.0.100:8080/";
-    private static final String baseURL = "http://192.168.0.100:8080/Server_war_exploded/";
+    //private static final String baseURL = "http://192.168.0.100:8080/Server_war_exploded/";
+
+    private static final String baseURL = "http://logrolling.sytes.net:8080/";
 
     public static String getBaseURL() {
         return baseURL;
